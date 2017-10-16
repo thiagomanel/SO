0d446abf43917a3e7b8f79c92fe7d0c435763139
Thiago Manel <thiago.manel@gmail.com>
2017-10-16 00:23:37 -0300

    add some comments

--- process/hello_world.asm
+++ process/hello_world.asm
@@ -1,5 +1,13 @@
 %define SYSCALL_WRITE 0x2000004
-  %define SYSCALL_EXIT  0x2000001
+%define SYSCALL_EXIT  0x2000001
+
+;sycall number is passed in register %rax.
+;rdi - used to pass 1st argument to functions
+;rsi - used to pass 2nd argument to functions
+;rdx - used to pass 3rd argument to functions
+;rcx - used to pass 4th argument to functions
+;r8 - used to pass 5th argument to functions
+;r9 - used to pass 6th argument to functions
 
 global start
 start:
