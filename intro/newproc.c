1cde5dc32b70909e5f45a561fd3c06478d74cbf5
Thiago Manel <thiago.manel@gmail.com>
2020-09-07 12:34:36 -0300

    format code

--- intro/newproc.c
+++ intro/newproc.c
@@ -11,12 +11,12 @@ int main() {
     pid = fork();
     if (pid == 0) {
     	printf("Upon successful completion, fork() returns a value of 0 to the child process\n");
-	printf("child dying\n");
-	exit(0);
+	    printf("child dying\n");
+	    exit(0);
     } else {
         printf("Upon successful completion, fork() returns returns the process ID of the child process to the parent process\n");
-	donep = wait(0);
-	printf("child %d has gone\n", donep);
+	    donep = wait(0);
+	    printf("child %d has gone\n", donep);
         return 0;
     }
 }
