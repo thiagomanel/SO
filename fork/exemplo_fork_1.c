02bb638c921830b87b3eb92232a99c68b2763805
fubica <fubica@dsc.ufcg.edu.br>
2020-09-15 18:59:43 -0300

    added fork examples

---
+++ fork/exemplo_fork_1.c
@@ -0,0 +1,18 @@
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int arrc, char** argv) {
+	char estado = 'p';
+    	int child_pid;
+
+	system("ps -f");
+    	printf("processo %d: criando processo filho\n", getpid());
+    	child_pid = fork();
+	system("ps -f");
+    	printf("processo %d: fork retornou %d\n", getpid(), child_pid);
+    	if (child_pid == 0) estado = 'f';
+    	printf("processo %d: o valor da variável estado deste processo é: %c\n", getpid(), estado);
+}
