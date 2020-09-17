f653db683966fbf005c28a55a2831d0ed6a99c1a
Francisco Vilar Brasileiro <fubica@computacao.ufcg.edu.br>
2020-09-17 11:43:04 -0300

    Added tsl version

---
+++ process/word_count/tsl_wc_thread.c
@@ -0,0 +1,164 @@
+//
+//  tsl_wc_thread.c
+//  
+//
+//  Created by Francisco Brasileiro on 17/09/20.
+//
+
+#include <ctype.h>
+#include <dirent.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define MAX_ANSWERS 1024
+
+struct thread_param {
+    char *dir_path;
+};
+
+size_t WC_COUNT;
+
+void enter_region() {
+    asm(
+        ".data\n\t"
+        "lock:\n\t"
+        ".byte 0\n\t"
+        ".text\n\t"
+        
+        "_enter_region:\n\t"
+        "movb $1, %al\n\t" /* move 1 to AL */
+        "xchgb (lock),%al\n\t"
+        "cmp $0, %al\n\t"
+        "jne _enter_region\n\t"
+        
+        );
+}
+
+void leave_region() {
+    asm("movb $0, (lock)");
+}
+
+size_t wc(const char *content) {
+    size_t count = 0;
+    
+    for (bool inword = false; *content; ++content) {
+        if (isspace(*content)) {
+            if (inword) {
+                inword = false;
+            }
+        } else {
+            if (!inword) {
+                inword = true;
+                ++count;
+            }
+        }
+    }
+    return count;
+}
+
+size_t wc_file(const char *filename) {
+    char *file_content = NULL;
+    long length;
+    
+    FILE *f = fopen(filename, "rb");
+    
+    if (f) {
+        fseek(f, 0, SEEK_END);
+        length = ftell(f);
+        fseek(f, 0, SEEK_SET);
+        file_content = malloc(length);
+        if (file_content) {
+            fread(file_content, 1, length, f);
+        }
+        fclose(f);
+        
+        return wc(file_content);
+    }
+    
+    return -1;
+}
+
+// count words in file within dir_path
+// we do not walk in subdirs
+void *wc_dir(void *t) {
+    struct thread_param *param = (struct thread_param*) t;
+    char *dir_path = param->dir_path;
+    
+    DIR *dir;
+    struct dirent *ent;
+    char *filepath;
+    size_t count;
+    
+    dir = opendir(dir_path);
+    if (dir) {
+        while ((ent = readdir(dir)) != NULL) {
+            if (ent->d_type == DT_REG) { // if is regular file
+                filepath = malloc(strlen(dir_path) + strlen(ent->d_name) + 2);
+                sprintf(filepath, "%s/%s", dir_path, ent->d_name);
+                count = wc_file(filepath);
+                enter_region();
+                WC_COUNT += count;
+                leave_region();
+                free(filepath);
+            }
+        }
+    }
+    closedir(dir);
+    
+    pthread_exit(NULL);
+}
+
+// it calculates the number of words in files
+// stored under directory argv[1]
+//
+// we assume a depth 3 hierarchy.
+// At level 1, we have the root
+// at level 2, we have subdirs
+// at level 3, we have files
+//
+// root--
+//       |-- subdir-1
+//       |-- subdir-2
+//       |-- ...
+//       |-- subdir-3
+int main(int argc, char *argv[argc + 1]) {
+    DIR *root_dir;
+    struct dirent *ent;
+    char *filepath[MAX_ANSWERS];
+    char *root_path = argv[1];
+    
+    WC_COUNT = 0;
+    
+    size_t n_threads = 0;
+    pthread_t threads[MAX_ANSWERS];
+    struct thread_param params[MAX_ANSWERS];
+    
+    root_dir = opendir(root_path);
+    
+    if (root_dir) {
+        while ((ent = readdir(root_dir)) != NULL) {
+            if (ent->d_type == DT_DIR) {
+                if (strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0) {
+                    filepath[n_threads] = malloc(strlen(root_path) + strlen(ent->d_name) + 2);
+                    sprintf(filepath[n_threads], "%s/%s", root_path, ent->d_name);
+                    params[n_threads].dir_path = filepath[n_threads];
+                    pthread_create(&threads[n_threads], NULL, wc_dir, (void *)&params[n_threads]);
+                    n_threads++;
+                }
+            }
+        }
+        closedir(root_dir);
+    }
+    
+    for (size_t i = 0; i < n_threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    
+    printf("%zu\n", WC_COUNT);
+    pthread_exit(NULL);
+}
