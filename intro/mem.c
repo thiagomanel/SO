369b4e1b0522d44341275a0a4b3e0524e23f7a80
thiagomanel <thiago.manel@gmail.com>
2024-07-30 10:51:47 -0300

    read from arg

--- intro/mem.c
+++ intro/mem.c
@@ -4,12 +4,11 @@
 #include <unistd.h>
 
 //cc -o mem mem.c -Wl,-no_pie
-
 int main(int argc, char *argv[]) {
     int *p = malloc(sizeof(int)); // a1
     printf("(%d) address pointed to by p: %p\n",
 		getpid(), p); // a2
-    *p = 0; // a3
+    *p = atoi(argv[1]);       // a3
     while (1) {
         sleep(1);
 	*p = *p + 1;
